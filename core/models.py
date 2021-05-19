58075230fd8df0c81dbe5196fd9428ccdd3f6a6f
Manu <Manu>
2021-05-19 12:40:00 +0000

    Routing images

--- core/models.py
+++ core/models.py
@@ -92,7 +92,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_superuser = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    profile_pic = models.ImageField(blank=True, null=True)
+    profile_pic = models.ImageField(blank=True, null=True, upload_to='profiles')
 
     objects = UserManager()
 
